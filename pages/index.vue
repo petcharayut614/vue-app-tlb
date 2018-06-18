9a1159873775f0d408c21f0c84e9c5c2b7e18ff6
unknown <jikke09338@gmail.com>
2018-06-18 10:05:19 +0700

    updateindex

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>Hello world</div>
+  <div>เพชรายุธ</div>
 </template>
 <script>
 export default {
   data() {
     return {
-      id: 1,
-    }
-  },
-}
+      id: 1
+    };
+  }
+};
 </script>
